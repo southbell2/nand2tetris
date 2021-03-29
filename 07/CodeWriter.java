93934ec89a1794d2b3a41f9ea2ce81213399ff35
southbell2 <southbell2357@naver.com>
2021-03-29 22:13:23 +0900

    CH07

---
+++ 07/CodeWriter.java
@@ -0,0 +1,496 @@
+package ch07;
+
+import java.io.*;
+
+public class CodeWriter {
+	private File asmFile;
+	private String asmFileName;
+	//어셈블리코드로 번역된 명령어들을 저장할 StringBuilder
+	private StringBuilder commands = new StringBuilder();
+	private int conditionCnt = 0;
+	
+	//생성자
+	public CodeWriter(String filePath) {
+		//asmFileName은 static의 pop,push 연산을 할때 쓸 파일이름
+		asmFileName = filePath;
+		for(int i = filePath.length()-1; 0 <= i; i--) {
+			if(filePath.charAt(i) == '/') {
+				asmFileName = filePath.substring(i+1);
+				break;
+			}
+		}
+		//filePath에는 .asm이 붙어있지 않아서 .asm을 붙여서 파일을 만든다
+		asmFile = new File(filePath + ".asm");
+	}
+	
+	//산술 및 논리 연산
+	void writerArithmetic(String segment) throws IOException {
+		if(segment.equals("add")) {
+			BufferedWriter writer = new BufferedWriter(new FileWriter(asmFile, true));
+			
+			commands.append("@SP").append('\n');
+			commands.append("AM=M-1").append('\n');
+			commands.append("D=M").append('\n');
+			commands.append("A=A-1").append('\n');
+			commands.append("M=D+M").append('\n');
+			
+			if(asmFile.isFile() && asmFile.canWrite()) {
+				writer.write(commands.toString());
+				writer.close();
+			}	
+			commands.setLength(0);
+		}
+		else if(segment.equals("sub")) {
+			BufferedWriter writer = new BufferedWriter(new FileWriter(asmFile, true));
+			
+			commands.append("@SP").append('\n');
+			commands.append("AM=M-1").append('\n');
+			commands.append("D=M").append('\n');
+			commands.append("A=A-1").append('\n');
+			commands.append("M=M-D").append('\n');
+			
+			if(asmFile.isFile() && asmFile.canWrite()) {
+				writer.write(commands.toString());
+				writer.close();
+			}	
+			commands.setLength(0);
+		}
+		else if(segment.equals("neg")) {
+			BufferedWriter writer = new BufferedWriter(new FileWriter(asmFile, true));
+			
+			commands.append("@SP").append('\n');
+			commands.append("A=M-1").append('\n');
+			commands.append("M=-M").append('\n');
+			
+			if(asmFile.isFile() && asmFile.canWrite()) {
+				writer.write(commands.toString());
+				writer.close();
+			}	
+			commands.setLength(0);
+		}
+		else if(segment.equals("eq")) {
+			BufferedWriter writer = new BufferedWriter(new FileWriter(asmFile, true));
+			
+			commands.append("@SP").append('\n');
+			commands.append("AM=M-1").append('\n');
+			commands.append("D=M").append('\n');
+			commands.append("A=A-1").append('\n');
+			commands.append("D=M-D").append('\n');
+			commands.append("M=-1").append('\n');
+			commands.append("@EQ").append(Integer.toString(conditionCnt)).append('\n');
+			commands.append("D;JEQ").append('\n');
+			commands.append("@SP").append('\n');
+			commands.append("A=M-1").append('\n');
+			commands.append("M=0").append('\n');
+			commands.append("(EQ").append(Integer.toString(conditionCnt)).append(')').append('\n');
+			
+			conditionCnt++;
+			
+			if(asmFile.isFile() && asmFile.canWrite()) {
+				writer.write(commands.toString());
+				writer.close();
+			}	
+			commands.setLength(0);
+		}
+		else if(segment.equals("gt")) {
+			BufferedWriter writer = new BufferedWriter(new FileWriter(asmFile, true));
+			
+			commands.append("@SP").append('\n');
+			commands.append("AM=M-1").append('\n');
+			commands.append("D=M").append('\n');
+			commands.append("A=A-1").append('\n');
+			commands.append("D=M-D").append('\n');
+			commands.append("M=-1").append('\n');
+			commands.append("@GT").append(Integer.toString(conditionCnt)).append('\n');
+			commands.append("D;JGT").append('\n');
+			commands.append("@SP").append('\n');
+			commands.append("A=M-1").append('\n');
+			commands.append("M=0").append('\n');
+			commands.append("(EQ").append(Integer.toString(conditionCnt)).append(')').append('\n');
+			
+			conditionCnt++;
+			
+			if(asmFile.isFile() && asmFile.canWrite()) {
+				writer.write(commands.toString());
+				writer.close();
+			}	
+			commands.setLength(0);
+		}
+		else if(segment.equals("lt")) {
+			BufferedWriter writer = new BufferedWriter(new FileWriter(asmFile, true));
+			
+			commands.append("@SP").append('\n');
+			commands.append("AM=M-1").append('\n');
+			commands.append("D=M").append('\n');
+			commands.append("A=A-1").append('\n');
+			commands.append("D=M-D").append('\n');
+			commands.append("M=-1").append('\n');
+			commands.append("@LT").append(Integer.toString(conditionCnt)).append('\n');
+			commands.append("D;JLT").append('\n');
+			commands.append("@SP").append('\n');
+			commands.append("A=M-1").append('\n');
+			commands.append("M=0").append('\n');
+			commands.append("(EQ").append(Integer.toString(conditionCnt)).append(')').append('\n');
+			
+			conditionCnt++;
+			
+			if(asmFile.isFile() && asmFile.canWrite()) {
+				writer.write(commands.toString());
+				writer.close();
+			}	
+			commands.setLength(0);
+		}
+		else if(segment.equals("and")) {
+			BufferedWriter writer = new BufferedWriter(new FileWriter(asmFile, true));
+			
+			commands.append("@SP").append('\n');
+			commands.append("AM=M-1").append('\n');
+			commands.append("D=M").append('\n');
+			commands.append("A=A-1").append('\n');
+			commands.append("M=D&M").append('\n');
+			
+			if(asmFile.isFile() && asmFile.canWrite()) {
+				writer.write(commands.toString());
+				writer.close();
+			}	
+			commands.setLength(0);
+		}
+		else if(segment.equals("or")) {
+			BufferedWriter writer = new BufferedWriter(new FileWriter(asmFile, true));
+			
+			commands.append("@SP").append('\n');
+			commands.append("AM=M-1").append('\n');
+			commands.append("D=M").append('\n');
+			commands.append("A=A-1").append('\n');
+			commands.append("M=D|M").append('\n');
+			
+			if(asmFile.isFile() && asmFile.canWrite()) {
+				writer.write(commands.toString());
+				writer.close();
+			}	
+			commands.setLength(0);
+		}
+		else if(segment.equals("not")) {
+			BufferedWriter writer = new BufferedWriter(new FileWriter(asmFile, true));
+			
+			commands.append("@SP").append('\n');
+			commands.append("A=M-1").append('\n');
+			commands.append("M=!M").append('\n');
+			
+			if(asmFile.isFile() && asmFile.canWrite()) {
+				writer.write(commands.toString());
+				writer.close();
+			}	
+			commands.setLength(0);
+		}
+	}
+	
+	//push, pop 연산
+	void writePushPop(int command, String segment, int index) throws IOException {
+		//push
+		if(command == Parser.C_PUSH) {
+			if(segment.equals("constant")) {
+				BufferedWriter writer = new BufferedWriter(new FileWriter(asmFile, true));
+				
+				commands.append('@').append(Integer.toString(index)).append('\n');
+				commands.append("D=A").append('\n');
+				commands.append("@SP").append('\n');
+				commands.append("A=M").append('\n');
+				commands.append("M=D").append('\n');
+				commands.append("@SP").append('\n');
+				commands.append("M=M+1").append('\n');
+				
+				if(asmFile.isFile() && asmFile.canWrite()) {
+					writer.write(commands.toString());
+					writer.close();
+				}			
+				commands.setLength(0);
+			}
+			else if(segment.equals("local")) {
+				BufferedWriter writer = new BufferedWriter(new FileWriter(asmFile, true));
+				
+				commands.append("@LCL").append('\n');
+				commands.append("D=M").append('\n');
+				commands.append('@').append(Integer.toString(index)).append('\n');
+				commands.append("A=D+A").append('\n');
+				commands.append("D=M").append('\n');
+				commands.append("@SP").append('\n');
+				commands.append("AM=M+1").append('\n');
+				commands.append("A=A-1").append('\n');
+				commands.append("M=D").append('\n');
+				
+				if(asmFile.isFile() && asmFile.canWrite()) {
+					writer.write(commands.toString());
+					writer.close();
+				}			
+				commands.setLength(0);			
+			}
+			else if(segment.equals("argument")) {
+				BufferedWriter writer = new BufferedWriter(new FileWriter(asmFile, true));
+				
+				commands.append("@ARG").append('\n');
+				commands.append("D=M").append('\n');
+				commands.append('@').append(Integer.toString(index)).append('\n');
+				commands.append("A=D+A").append('\n');
+				commands.append("D=M").append('\n');
+				commands.append("@SP").append('\n');
+				commands.append("AM=M+1").append('\n');
+				commands.append("A=A-1").append('\n');
+				commands.append("M=D").append('\n');
+				
+				if(asmFile.isFile() && asmFile.canWrite()) {
+					writer.write(commands.toString());
+					writer.close();
+				}			
+				commands.setLength(0);			
+			}
+			else if(segment.equals("this")) {
+				BufferedWriter writer = new BufferedWriter(new FileWriter(asmFile, true));
+				
+				commands.append("@THIS").append('\n');
+				commands.append("D=M").append('\n');
+				commands.append('@').append(Integer.toString(index)).append('\n');
+				commands.append("A=D+A").append('\n');
+				commands.append("D=M").append('\n');
+				commands.append("@SP").append('\n');
+				commands.append("AM=M+1").append('\n');
+				commands.append("A=A-1").append('\n');
+				commands.append("M=D").append('\n');
+				
+				if(asmFile.isFile() && asmFile.canWrite()) {
+					writer.write(commands.toString());
+					writer.close();
+				}			
+				commands.setLength(0);			
+			}
+			else if(segment.equals("that")) {
+				BufferedWriter writer = new BufferedWriter(new FileWriter(asmFile, true));
+				
+				commands.append("@THAT").append('\n');
+				commands.append("D=M").append('\n');
+				commands.append('@').append(Integer.toString(index)).append('\n');
+				commands.append("A=D+A").append('\n');
+				commands.append("D=M").append('\n');
+				commands.append("@SP").append('\n');
+				commands.append("AM=M+1").append('\n');
+				commands.append("A=A-1").append('\n');
+				commands.append("M=D").append('\n');
+				
+				if(asmFile.isFile() && asmFile.canWrite()) {
+					writer.write(commands.toString());
+					writer.close();
+				}			
+				commands.setLength(0);			
+			}
+			else if(segment.equals("temp")) {
+				BufferedWriter writer = new BufferedWriter(new FileWriter(asmFile, true));
+				
+				commands.append("@R5").append('\n');
+				commands.append("D=A").append('\n');
+				commands.append('@').append(Integer.toString(index)).append('\n');
+				commands.append("A=D+A").append('\n');
+				commands.append("D=M").append('\n');
+				commands.append("@SP").append('\n');
+				commands.append("AM=M+1").append('\n');
+				commands.append("A=A-1").append('\n');
+				commands.append("M=D").append('\n');
+				
+				if(asmFile.isFile() && asmFile.canWrite()) {
+					writer.write(commands.toString());
+					writer.close();
+				}			
+				commands.setLength(0);			
+			}
+			else if(segment.equals("pointer")) {
+				BufferedWriter writer = new BufferedWriter(new FileWriter(asmFile, true));
+				
+				commands.append("@R3").append('\n');
+				commands.append("D=A").append('\n');
+				commands.append('@').append(Integer.toString(index)).append('\n');
+				commands.append("A=D+A").append('\n');
+				commands.append("D=M").append('\n');
+				commands.append("@SP").append('\n');
+				commands.append("AM=M+1").append('\n');
+				commands.append("A=A-1").append('\n');
+				commands.append("M=D").append('\n');
+				
+				if(asmFile.isFile() && asmFile.canWrite()) {
+					writer.write(commands.toString());
+					writer.close();
+				}			
+				commands.setLength(0);			
+			}
+			else if(segment.equals("static")) {
+				BufferedWriter writer = new BufferedWriter(new FileWriter(asmFile, true));
+				
+				commands.append('@').append(asmFileName).append('.').append(Integer.toString(index)).append('\n');
+				commands.append("D=M").append('\n');
+				commands.append("@SP").append('\n');
+				commands.append("AM=M+1").append('\n');
+				commands.append("A=A-1").append('\n');
+				commands.append("M=D").append('\n');
+				
+				if(asmFile.isFile() && asmFile.canWrite()) {
+					writer.write(commands.toString());
+					writer.close();
+				}			
+				commands.setLength(0);			
+			}
+		}
+		//pop
+		else {
+			if(segment.equals("local")) {
+				BufferedWriter writer = new BufferedWriter(new FileWriter(asmFile, true));
+				
+				commands.append("@LCL").append('\n');
+				commands.append("D=M").append('\n');
+				commands.append('@').append(Integer.toString(index)).append('\n');
+				commands.append("D=D+A").append('\n');
+				commands.append("@R13").append('\n');
+				commands.append("M=D").append('\n');
+				commands.append("@SP").append('\n');
+				commands.append("AM=M-1").append('\n');
+				commands.append("D=M").append('\n');
+				commands.append("@R13").append('\n');
+				commands.append("A=M").append('\n');
+				commands.append("M=D").append('\n');
+				
+				if(asmFile.isFile() && asmFile.canWrite()) {
+					writer.write(commands.toString());
+					writer.close();
+				}			
+				commands.setLength(0);
+			}
+			else if(segment.equals("argument")) {
+				BufferedWriter writer = new BufferedWriter(new FileWriter(asmFile, true));
+				
+				commands.append("@ARG").append('\n');
+				commands.append("D=M").append('\n');
+				commands.append('@').append(Integer.toString(index)).append('\n');
+				commands.append("D=D+A").append('\n');
+				commands.append("@R13").append('\n');
+				commands.append("M=D").append('\n');
+				commands.append("@SP").append('\n');
+				commands.append("AM=M-1").append('\n');
+				commands.append("D=M").append('\n');
+				commands.append("@R13").append('\n');
+				commands.append("A=M").append('\n');
+				commands.append("M=D").append('\n');
+				
+				if(asmFile.isFile() && asmFile.canWrite()) {
+					writer.write(commands.toString());
+					writer.close();
+				}			
+				commands.setLength(0);
+			}
+			else if(segment.equals("this")) {
+				BufferedWriter writer = new BufferedWriter(new FileWriter(asmFile, true));
+				
+				commands.append("@THIS").append('\n');
+				commands.append("D=M").append('\n');
+				commands.append('@').append(Integer.toString(index)).append('\n');
+				commands.append("D=D+A").append('\n');
+				commands.append("@R13").append('\n');
+				commands.append("M=D").append('\n');
+				commands.append("@SP").append('\n');
+				commands.append("AM=M-1").append('\n');
+				commands.append("D=M").append('\n');
+				commands.append("@R13").append('\n');
+				commands.append("A=M").append('\n');
+				commands.append("M=D").append('\n');
+				
+				if(asmFile.isFile() && asmFile.canWrite()) {
+					writer.write(commands.toString());
+					writer.close();
+				}			
+				commands.setLength(0);
+			}
+			else if(segment.equals("that")) {
+				BufferedWriter writer = new BufferedWriter(new FileWriter(asmFile, true));
+				
+				commands.append("@THAT").append('\n');
+				commands.append("D=M").append('\n');
+				commands.append('@').append(Integer.toString(index)).append('\n');
+				commands.append("D=D+A").append('\n');
+				commands.append("@R13").append('\n');
+				commands.append("M=D").append('\n');
+				commands.append("@SP").append('\n');
+				commands.append("AM=M-1").append('\n');
+				commands.append("D=M").append('\n');
+				commands.append("@R13").append('\n');
+				commands.append("A=M").append('\n');
+				commands.append("M=D").append('\n');
+				
+				if(asmFile.isFile() && asmFile.canWrite()) {
+					writer.write(commands.toString());
+					writer.close();
+				}			
+				commands.setLength(0);
+			}
+			else if(segment.equals("temp")) {
+				BufferedWriter writer = new BufferedWriter(new FileWriter(asmFile, true));
+				
+				commands.append("@R5").append('\n');
+				commands.append("D=A").append('\n');
+				commands.append('@').append(Integer.toString(index)).append('\n');
+				commands.append("D=D+A").append('\n');
+				commands.append("@R13").append('\n');
+				commands.append("M=D").append('\n');
+				commands.append("@SP").append('\n');
+				commands.append("AM=M-1").append('\n');
+				commands.append("D=M").append('\n');
+				commands.append("@R13").append('\n');
+				commands.append("A=M").append('\n');
+				commands.append("M=D").append('\n');
+				
+				if(asmFile.isFile() && asmFile.canWrite()) {
+					writer.write(commands.toString());
+					writer.close();
+				}			
+				commands.setLength(0);
+			}
+			else if(segment.equals("pointer")) {
+				BufferedWriter writer = new BufferedWriter(new FileWriter(asmFile, true));
+				
+				commands.append("@R3").append('\n');
+				commands.append("D=A").append('\n');
+				commands.append('@').append(Integer.toString(index)).append('\n');
+				commands.append("D=D+A").append('\n');
+				commands.append("@R13").append('\n');
+				commands.append("M=D").append('\n');
+				commands.append("@SP").append('\n');
+				commands.append("AM=M-1").append('\n');
+				commands.append("D=M").append('\n');
+				commands.append("@R13").append('\n');
+				commands.append("A=M").append('\n');
+				commands.append("M=D").append('\n');
+				
+				if(asmFile.isFile() && asmFile.canWrite()) {
+					writer.write(commands.toString());
+					writer.close();
+				}			
+				commands.setLength(0);
+			}
+			else if(segment.equals("static")) {
+				BufferedWriter writer = new BufferedWriter(new FileWriter(asmFile, true));
+				
+				commands.append('@').append(asmFileName).append('.').append(Integer.toString(index)).append('\n');
+				commands.append("D=A").append('\n');
+				commands.append("@R13").append('\n');
+				commands.append("M=D").append('\n');
+				commands.append("@SP").append('\n');
+				commands.append("AM=M-1").append('\n');
+				commands.append("D=M").append('\n');
+				commands.append("@R13").append('\n');
+				commands.append("A=M").append('\n');
+				commands.append("M=D").append('\n');
+				
+				if(asmFile.isFile() && asmFile.canWrite()) {
+					writer.write(commands.toString());
+					writer.close();
+				}			
+				commands.setLength(0);
+			}
+		}
+	}
+}
---
+++ 07/Parser.java
@@ -0,0 +1,102 @@
+package ch07;
+import java.io.*;
+import java.util.*;
+
+//VM의 Parser 모듈, 구문 분석을 한다
+public class Parser {
+	
+	public Scanner sc;
+	private String command;
+	private String arg1;
+	private String arg2;
+	
+	public final static int C_ARITHMETIC = 0;
+	public final static int C_PUSH = 1;
+	public final static int C_POP = 2;
+	public final static int C_LABLE = 3;
+	public final static int C_GOTO = 4;
+	public final static int C_IF = 5;
+	public final static int C_FUNCTION = 6;
+	public final static int C_RETURN = 7;
+	public final static int C_CALL = 8;
+	
+	//생성자
+	public Parser(String filePath) {
+		try {
+			File vmFile = new File(filePath);
+			sc = new Scanner(vmFile);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	public Parser(File vmFile) {
+		try {
+			sc = new Scanner(vmFile);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	boolean hasMoreCommands() {
+		return sc.hasNextLine();
+	}
+
+	//입력에서 다음 명령을 읽어서 현재 명령으로 만든다
+	void advance() {
+		if(hasMoreCommands()) {
+			String nowCommand = sc.nextLine();		
+			//주석 제거하기
+			for(int i = 0; i < nowCommand.length(); i++) {
+				if(nowCommand.charAt(i) == '/') {
+					nowCommand = nowCommand.substring(0, i);
+				}
+			}		
+			//빈 줄일 경우 건너뛰기
+			if(nowCommand.length() == 0) {
+				this.advance();
+				return;
+			}
+
+			//현재 받은 문자열을 command,arg1,arg2로 분리하기
+			StringTokenizer st = new StringTokenizer(nowCommand);
+			command = st.nextToken();
+			if(st.hasMoreTokens()) arg1 = st.nextToken();
+			else arg1 = "";
+			if(st.hasMoreTokens()) arg2 = st.nextToken();
+			else arg2 = "";
+		}
+		else {
+			System.out.println("No more command");
+		}
+	}
+	
+	//명령의 타입을 반환한다
+	int commandType() {
+		if(command.equals("add") || command.equals("sub") || command.equals("neg") ||
+		   command.equals("eq") || command.equals("gt") || command.equals("lt") ||
+		   command.equals("and") || command.equals("or") || command.equals("not")) {
+			return C_ARITHMETIC;
+		}
+		else if(command.equals("push")) {
+			return C_PUSH;
+		}
+		else if(command.equals("pop")) {
+			return C_POP;
+		}
+		return -1;
+	}
+	
+	//arg1
+	String arg1() {
+		if(commandType() == C_ARITHMETIC) {
+			return command;
+		}
+		
+		return arg1;
+	}
+	
+	//arg2
+	int arg2() {
+		return Integer.parseInt(arg2);
+	}
+}
